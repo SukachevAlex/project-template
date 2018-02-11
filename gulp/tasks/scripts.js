3f1b1f33ee4a94a91bfc890c54ec1ca306fd5c74
SukachevAlex <sukachevalexandr@gmail.com>
2018-02-11 23:12:16 +0300

    fix task with js libs

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -1,7 +1,7 @@
 module.exports = function () {
-    $.gulp.task('scripts:lib', function(){
+
+    $.gulp.task('libs:dev', function(){
         return $.gulp.src([
-            'node_modules/jquery/dist/jquery.min.js',
             'node_modules/slick-carousel/slick/slick.min.js',
         ])
             .pipe($.gp.concat('libs.min.js'))
@@ -11,8 +11,20 @@ module.exports = function () {
             }));
     });
 
+    $.gulp.task('libs:build', function(){
+        return $.gulp.src([
+            'node_modules/slick-carousel/slick/slick.min.js',
+        ])
+            .pipe($.gp.concat('libs.min.js'))
+            .pipe($.gp.uglifyjs())
+            .pipe($.gulp.dest('dist/js/'));
+    });
+
     $.gulp.task('scripts', function(){
-        return $.gulp.src('app/scripts/main.js')
+        return $.gulp.src([
+            'app/scripts/*.js',
+            'app/scripts/libs.min.js'
+        ])
             .pipe($.gulp.dest('dist/js/'))
             .pipe($.browserSync.reload({
                 stream: true
--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
 'use strict';
 
 global.$ = {
-  gulp        : require('gulp'),
-  gp          : require('gulp-load-plugins')(),
-  browserSync : require('browser-sync').create(),
-  del	      : require('del'),
+    gulp        : require('gulp'),
+    gp          : require('gulp-load-plugins')(),
+    browserSync : require('browser-sync').create(),
+    del	      : require('del'),
 
-  path : {
-      tasks   : require('./gulp/config/tasks.js')
-  }
+    path : {
+        tasks   : require('./gulp/config/tasks.js')
+    }
 };
 
 $.path.tasks.forEach(function (taskPath) {
@@ -17,17 +17,17 @@ $.path.tasks.forEach(function (taskPath) {
 
 $.gulp.task('dev', $.gulp.series(
     'clean',
-    $.gulp.parallel('sass:dev', 'pug', 'scripts', 'img:dev', 'fonts')));
+    $.gulp.parallel('sass:dev', 'pug', 'libs:dev', 'scripts', 'img:dev', 'fonts')));
 
 
 $.gulp.task('build', $.gulp.series(
     'clean',
-    $.gulp.parallel('pug', 'fonts' ,'sass:build', 'scripts:lib', 'scripts', 'img:build'),
+    $.gulp.parallel('pug', 'fonts' ,'sass:build', 'libs:build', 'scripts', 'img:build'),
     $.gulp.parallel('watch', 'browser-sync')
 ));
 
 $.gulp.task('default', $.gulp.series(
     'dev',
-     $.gulp.parallel('watch', 'browser-sync')
+    $.gulp.parallel('watch', 'browser-sync')
 ));
 
